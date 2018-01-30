33d55aba36e8488a0ab17bbf8d346388a016d3f8
Iqbal Hussain <lincoln@live.in>
2018-01-30 17:50:35 +0530

    Completed

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound
+from django.db.models import Sum, Avg
 
 from .models import *
 
@@ -23,8 +24,38 @@ def site_category_details(request, site_category_id):
 
 
 def summary_sum(request):
-    return render(request, 'main/summary_sum.html')
+    site_category_data = []
+
+    for site_category in SiteCategory.objects.all():
+        sum_a_dict = Site.objects.filter(category=site_category).aggregate(Sum('a'))
+        sum_a = sum_a_dict[next(iter(sum_a_dict))]
+
+        sum_b_dict = Site.objects.filter(category=site_category).aggregate(Sum('b'))
+        sum_b = sum_b_dict[next(iter(sum_b_dict))]
+
+        site_category_data.append({
+            'name': site_category.name,
+            'a_value': sum_a,
+            'b_value': sum_b
+        })
+
+    return render(request, 'main/summary.html', {'site_category_data': site_category_data})
 
 
 def summary_avg(request):
-    return render(request, 'main/summary_avg.html')
+    site_category_data = []
+
+    for site_category in SiteCategory.objects.all():
+        avg_a_dict = Site.objects.filter(category=site_category).aggregate(Avg('a'))
+        avg_a = avg_a_dict[next(iter(avg_a_dict))]
+
+        avg_b_dict = Site.objects.filter(category=site_category).aggregate(Avg('b'))
+        avg_b = avg_b_dict[next(iter(avg_b_dict))]
+
+        site_category_data.append({
+            'name': site_category.name,
+            'a_value': avg_a,
+            'b_value': avg_b
+        })
+
+    return render(request, 'main/summary.html', {'site_category_data': site_category_data})
