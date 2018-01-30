66b911f68f04a3bd4cb7ea21d4ca8908d30cabb6
Iqbal Hussain <lincoln@live.in>
2018-01-30 22:14:52 +0530

    Final Commit

--- django_3mw/production.py
+++ django_3mw/production.py
@@ -13,7 +13,6 @@ DATABASES = {'default': dj_database_url.config()}
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
